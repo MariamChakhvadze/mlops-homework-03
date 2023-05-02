ba42f99b898bd5c090c181884820de1e138e0222
MariamChakhvadze <mariam.chakhvadze@quantori.com>
2023-05-02 15:38:25 +0400

    Create a project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project contains my solutions for homework #3 of the Quantori MLOps course',
+    author='Mariami Chakhvadze',
+    license='MIT',
+)

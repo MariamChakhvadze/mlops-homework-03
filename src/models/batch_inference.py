adaaf523fbbed37400bddcf58750d465f433dce9
MariamChakhvadze <mariam.chakhvadze@quantori.com>
2023-05-10 20:14:52 +0400

    Modify batch inference

--- src/models/batch_inference.py
+++ src/models/batch_inference.py
@@ -8,13 +8,13 @@ def batch_predict(data_filepath: str, model_uri: str) -> NDArray[float]:
     """Make a batch prediction.
 
     Args:
-        data_filepath: file path of processed data.
+        data_filepath: file path of processed data (CSV file).
         model_uri: model URI.
 
     Returns:
         Predictions as NumPy array.
     """
-    data = pd.read_parquet(data_filepath)
+    data = pd.read_csv(data_filepath)
 
     model = mlflow.pyfunc.load_model(model_uri)
 
